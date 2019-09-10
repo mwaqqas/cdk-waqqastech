9ef68a8736be359210009df3ed91cd7085746c34
mwaqqas <mohamed.waqqas@gmail.com>
2019-09-10 17:17:43 +0300

    Multiple Changes. Restructured Import statements. Added Stack Tags.  Started using CONSTANTS for better resusability. ACM, CLOUDFRONT, Route53 resources added. Stack is now complete for v1 functionality.

--- waqqastech/waqqastech_stack.py
+++ waqqastech/waqqastech_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import core
-from aws_cdk import aws_s3, aws_iam, aws_cloudfront, aws_route53
+from aws_cdk import aws_s3, aws_iam, aws_cloudfront
+from aws_cdk import aws_route53, aws_route53_targets
 from aws_cdk import aws_certificatemanager
+from constants import *
 
 
 class WaqqastechStack(core.Stack):
@@ -8,26 +10,28 @@ class WaqqastechStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Common Stack Tags
+        for k, v in COMMON_TAGS.items():
+            core.Tag.add(self, key=k, value=v)
+
         # Hosted Zone
         hosted_zone = aws_route53.HostedZone(
             self,
-            "ApexHostedZone",
-            zone_name="waqqas.tech",
-            comment="waqqas.tech",
+            "MainHostedZone",
+            zone_name=HOSTED_ZONE_NAME,
+            comment="Hosted Zone for {}".format(HOSTED_ZONE_NAME),
         )
 
         # ACM Certificate
-        # acm_certificate = aws_certificatemanager.DnsValidatedCertificate(
-        #     self,
-        #     "CloudFrontCertificate",
-        #     hosted_zone=hosted_zone,
-        #     region="us-east-1",
-        #     domain_name="waqqas.tech.",
-        #     subject_alternative_names=[
-        #         "blog.waqqas.tech"
-        #     ],
-        #     validation_method=aws_certificatemanager.ValidationMethod.DNS
-        # )
+        acm_certificate = aws_certificatemanager.DnsValidatedCertificate(
+            self,
+            "CloudFrontCertificate",
+            hosted_zone=hosted_zone,
+            region=CERTIFICATE_REGION,
+            domain_name=DOMAIN_NAME,
+            subject_alternative_names=CERTIFICATE_ALT_DOMAIN_NAMES,
+            validation_method=aws_certificatemanager.ValidationMethod.DNS
+        )
 
         # Website Bucket
         website_bucket = aws_s3.Bucket(
@@ -42,7 +46,7 @@ class WaqqastechStack(core.Stack):
             self,
             "CloudfrontOAI",
             cloud_front_origin_access_identity_config=aws_cloudfront.CfnCloudFrontOriginAccessIdentity.CloudFrontOriginAccessIdentityConfigProperty(
-                comment="abc"
+                comment="CloudFrontOAIFor{}".format(PROJECT_CODE.capitalize())
             )
         )
 
@@ -60,38 +64,50 @@ class WaqqastechStack(core.Stack):
                 effect=aws_iam.Effect.ALLOW
             )
         )
+
         # CloudFront Web Distribution
-        # cloudfront_distribution = aws_cloudfront.CloudFrontWebDistribution(
-        #     self,
-        #     "CloudFrontDistribution",
-        #     comment="This is a comment",
-        #     default_root_object="index.html",
-        #     viewer_protocol_policy=aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        #     alias_configuration=aws_cloudfront.AliasConfiguration(
-        #         acm_cert_ref="arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012",
-        #         security_policy=aws_cloudfront.SecurityPolicyProtocol.TLS_V1_2_2018,
-        #         names=[
-        #             "waqqas.tech",
-        #             "blog.waqqas.tech"
-        #         ]
-        #     ),
-        #     origin_configs=[
-        #         aws_cloudfront.SourceConfiguration(
-        #             s3_origin_source=aws_cloudfront.S3OriginConfig(
-        #                 s3_bucket_source=website_bucket,
-        #                 origin_access_identity_id=website_bucket_oai.attr_s3_canonical_user_id
-        #             ),
-        #             origin_path="/",
-        #             behaviors=[
-        #                 aws_cloudfront.Behavior(
-        #                     allowed_methods=aws_cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
-        #                     cached_methods=aws_cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
-        #                     compress=True,
-        #                     is_default_behavior=True,
-        #                     path_pattern="*",
-        #                     default_ttl=core.Duration.seconds(amount=3600),
-        #                 )
-        #             ]
-        #         )
-        #     ]
-        # )
+        cloudfront_distribution = aws_cloudfront.CloudFrontWebDistribution(
+            self,
+            "CloudFrontDistribution",
+            comment="waqqas.tech",
+            default_root_object="index.html",
+            viewer_protocol_policy=aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            alias_configuration=aws_cloudfront.AliasConfiguration(
+                acm_cert_ref=acm_certificate.certificate_arn,
+                security_policy=aws_cloudfront.SecurityPolicyProtocol.TLS_V1_2_2018,
+                names=CLOUDFRONT_DIST_ALT_DOMAINS
+            ),
+            origin_configs=[
+                aws_cloudfront.SourceConfiguration(
+                    s3_origin_source=aws_cloudfront.S3OriginConfig(
+                        s3_bucket_source=website_bucket,
+                        origin_access_identity_id=website_bucket_oai.ref
+                    ),
+                    behaviors=[
+                        aws_cloudfront.Behavior(
+                            allowed_methods=aws_cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+                            cached_methods=aws_cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
+                            compress=True,
+                            is_default_behavior=True,
+                            path_pattern="*",
+                            default_ttl=core.Duration.seconds(amount=60),
+                        )
+                    ]
+                )
+            ]
+        )
+
+        # CloudFront Route53 Record
+        apex_domain_dns_record = aws_route53.ARecord(
+            self,
+            "PrimaryAliasDNSRecord",
+            zone=hosted_zone,
+            comment="{} CloudFront Dist Alias Record".format(PROJECT_CODE),
+            record_name="{}.".format(DOMAIN_NAME),
+            target=aws_route53.RecordTarget.from_alias(
+                aws_route53_targets.CloudFrontTarget(
+                    cloudfront_distribution
+                )
+            ),
+            ttl=core.Duration.seconds(amount=CLOUDFRONT_DEFAULT_TTL),
+        )
