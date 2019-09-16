7fa9600c7c5c9a89a198d8416f24bd50a1985a0a
mwaqqas <mohamed.waqqas@gmail.com>
2019-09-16 22:00:24 +0300

    CodeBuild done

--- waqqastech/waqqastech_stack.py
+++ waqqastech/waqqastech_stack.py
@@ -2,7 +2,9 @@ from aws_cdk import core
 from aws_cdk import aws_s3, aws_iam, aws_cloudfront
 from aws_cdk import aws_route53, aws_route53_targets
 from aws_cdk import aws_certificatemanager
-import constants as vars
+from aws_cdk import aws_codebuild
+import constants
+import buildspec
 
 
 class WaqqastechStack(core.Stack):
@@ -13,40 +15,40 @@ class WaqqastechStack(core.Stack):
 
         print()
         # Common Stack Tags
-        for k, v in vars.COMMON_TAGS.items():
+        for k, v in constants.COMMON_TAGS.items():
             core.Tag.add(self, key=k, value=v)
 
         # Hosted Zone
-        if vars.HOSTED_ZONE["id"]:
+        if constants.HOSTED_ZONE["id"]:
             hosted_zone = aws_route53.HostedZone.from_hosted_zone_attributes(
                 self,
                 "ImportedHostedZone",
-                hosted_zone_id=vars.HOSTED_ZONE["id"],
-                zone_name=vars.HOSTED_ZONE["name"]
+                hosted_zone_id=constants.HOSTED_ZONE["id"],
+                zone_name=constants.HOSTED_ZONE["name"]
             )
         else:
             hosted_zone = aws_route53.HostedZone(
                 self,
                 "MainHostedZone",
-                zone_name=vars.HOSTED_ZONE["name"],
-                comment="Hosted Zone for {}".format(vars.HOSTED_ZONE["name"]),
+                zone_name=constants.HOSTED_ZONE["name"],
+                comment="Hosted Zone for {}".format(constants.HOSTED_ZONE["name"]),
             )
 
         # ACM Certificate
-        if vars.CERTIFICATE["arn"]:
+        if constants.CERTIFICATE["arn"]:
             acm_certificate = aws_certificatemanager.Certificate.from_certificate_arn(
                 self,
                 "ImportedCertificate",
-                certificate_arn=vars.CERTIFICATE["arn"]
+                certificate_arn=constants.CERTIFICATE["arn"]
             )
         else:
             acm_certificate = aws_certificatemanager.DnsValidatedCertificate(
                 self,
                 "CloudFrontCertificate",
                 hosted_zone=hosted_zone,
-                region=vars.CERTIFICATE["region"],
-                domain_name=vars.HOSTED_ZONE["domain"],
-                subject_alternative_names=vars.CERTIFICATE["alt_domains"],
+                region=constants.CERTIFICATE["region"],
+                domain_name=constants.HOSTED_ZONE["domain"],
+                subject_alternative_names=constants.CERTIFICATE["alt_domains"],
                 validation_method=aws_certificatemanager.ValidationMethod.DNS
             )
             acm_certificate.node.add_dependency(hosted_zone)
@@ -67,7 +69,7 @@ class WaqqastechStack(core.Stack):
             .CfnCloudFrontOriginAccessIdentity
             .CloudFrontOriginAccessIdentityConfigProperty(
                 comment="CloudFrontOAIFor{}".format(
-                    vars.PROJECT_CODE.capitalize()
+                    constants.PROJECT_CODE.capitalize()
                 )
             )
         )
@@ -99,7 +101,7 @@ class WaqqastechStack(core.Stack):
                 acm_cert_ref=acm_certificate.certificate_arn,
                 security_policy=aws_cloudfront
                 .SecurityPolicyProtocol.TLS_V1_2_2018,
-                names=vars.CLOUDFRONT["alt_domains"]
+                names=constants.CLOUDFRONT["alt_domains"]
             ),
             origin_configs=[
                 aws_cloudfront.SourceConfiguration(
@@ -128,14 +130,45 @@ class WaqqastechStack(core.Stack):
             self,
             "PrimaryDNSRecord",
             zone=hosted_zone,
-            comment="{} CloudFront Dist Alias Record".format(vars.PROJECT_CODE),
-            record_name="{}.".format(vars.HOSTED_ZONE["domain"]),
+            comment="{} CloudFront Dist Alias Record".format(constants.PROJECT_CODE),
+            record_name="{}.".format(constants.HOSTED_ZONE["domain"]),
             target=aws_route53.RecordTarget.from_alias(
                 aws_route53_targets.CloudFrontTarget(
                     cloudfront_distribution
                 )
             ),
-            ttl=core.Duration.seconds(amount=vars.CLOUDFRONT["default_ttl"]),
+            ttl=core.Duration.seconds(amount=constants.CLOUDFRONT["default_ttl"]),
+        )
+
+        # Artifact Bucket
+        artifact_bucket = aws_s3.Bucket(
+            self,
+            "ArtifactBucket",
+            encryption=aws_s3.BucketEncryption.S3_MANAGED,
+            removal_policy=core.RemovalPolicy.DESTROY
         )
 
         # CodeBuild
+        codebuild_environment_variables = aws_codebuild.BuildEnvironmentVariable(
+            value=website_bucket.bucket_name
+        )
+        codebuild_environment = aws_codebuild.BuildEnvironment(
+            build_image=aws_codebuild.LinuxBuildImage.UBUNTU_14_04_PYTHON_3_7_1,
+            compute_type=aws_codebuild.ComputeType.SMALL
+        )
+        codebuild_buildspec = aws_codebuild.BuildSpec.from_object(
+            value=buildspec.BUILDSPEC
+        )
+        codebuild_project = aws_codebuild.PipelineProject(
+            self,
+            "CodeBuildProject",
+            environment_variables={
+                "BUCKET_NAME": codebuild_environment_variables
+            },
+            environment=codebuild_environment,
+            build_spec=codebuild_buildspec,
+            description="CodeBuild Project for {} Content".format(constants.PROJECT_CODE),
+            timeout=core.Duration.seconds(amount=180)
+        )
+
+        # Codepipeline
